2bb737770b6419fec06821f92e0d7999ca3d6331
Konrad Bujas <kondziobu@gmail.com>
2021-08-12 10:13:57 +0200

    fix: double click to change mode

--- main.js
+++ main.js
@@ -2,12 +2,13 @@ let isDark = false ;
 const switchModes = document.querySelector('.mode--js');
 switchModes.addEventListener('click', () => {
     if (isDark) {
+        
         document.documentElement.style.setProperty('--background-color','#fefefe');
         document.documentElement.style.setProperty('--text-color','#333333');
         isDark = false;
     } else {
-        document.documentElement.style.setProperty('--background-color','#333333');
-        document.documentElement.style.setProperty('--text-color','#fefefe');
+        document.documentElement.style.setProperty('--background-color','#37383b');
+        document.documentElement.style.setProperty('--text-color','#8f8a8a');
         isDark = true;
     }
 })
