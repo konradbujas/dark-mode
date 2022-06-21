9d7ae1646f14919009dfb7d1628917cbbd73cde3
Konrad Bujas <kondziobu@gmail.com>
2022-06-21 09:08:59 +0200

    button mode style

--- main.js
+++ main.js
@@ -1,14 +1,29 @@
+// let isDark = false ;
+// const switchModes = document.querySelector('.mode--js');
+// switchModes.addEventListener('click', () => {
+//     if (isDark) {
+        
+//         document.documentElement.style.setProperty('--background-color','#fefefe');
+//         document.documentElement.style.setProperty('--text-color','#333333');
+//         isDark = false;
+//     } else {
+//         document.documentElement.style.setProperty('--background-color','#37383b');
+//         document.documentElement.style.setProperty('--text-color','#8f8a8a');
+//         isDark = true;
+//     }
+// })
+
 let isDark = false ;
 const switchModes = document.querySelector('.mode--js');
 switchModes.addEventListener('click', () => {
     if (isDark) {
-        
-        document.documentElement.style.setProperty('--background-color','#fefefe');
-        document.documentElement.style.setProperty('--text-color','#333333');
+        document.querySelector('body').style.backgroundColor = '#000';
+        document.querySelector('body').style.color = '#fff';
         isDark = false;
     } else {
-        document.documentElement.style.setProperty('--background-color','#37383b');
-        document.documentElement.style.setProperty('--text-color','#8f8a8a');
+        document.querySelector('body').style.backgroundColor = '#fff';
+        document.querySelector('body').style.color = '#000';
         isDark = true;
     }
 })
+
